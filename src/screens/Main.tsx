eaeb758bedcb4417b800cb5e3c67f6d6110d235d
namghjk <namsgu123@gmail.com>
2023-08-04 00:33:41 +0700

    add TabBottom Main Screen

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,12 +1,67 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, {useState} from 'react';
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 const Main = () => {
+  const [didTabSelected, SetDidTabSelect] = useState(0);
   return (
-    <View>
-        <Text>Main</Text>
-    </View>
-  )
-}
+    <SafeAreaView style={{flex: 1}}>
+      <View style={{flex: 1, backgroundColor: 'red'}}>
+        <View style={styles.bottomTab}>
+          <TouchableOpacity style={styles.tab} onPress={()=>{SetDidTabSelect(0)}}>
+            <Image
+              source={require('../../Access/Images/group.png')}
+              style={[
+                styles.tabIcon,
+                {
+                  width: 40,
+                  height: 40,
+                  tintColor: didTabSelected == 0 ? 'white' : 'gray',
+                },
+              ]}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tab} onPress={()=>{SetDidTabSelect(1)}}>
+            <Image
+              source={require('../../Access/Images/settingIcon.png')}
+              style={[
+                styles.tabIcon,
+                {tintColor: didTabSelected == 1 ? 'white' : 'gray'},
+              ]}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default Main
+export default Main;
+const styles = StyleSheet.create({
+  bottomTab: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    height: 70,
+    backgroundColor: 'purple',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  tab: {
+    width: '50%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tabIcon: {
+    width: 35,
+    height: 35,
+  },
+});
